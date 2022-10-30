ca6e89238ad4447f468fe56277b5e8d2dba89c26
FizaSrafraz04 <bitf21m027@pucit.edu.pk>
2022-10-30 19:06:15 +0500

    task 1 done

---
+++ AirlineResevationSystem.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<string.h>
+int main(void) {
+	int select;
+	int a;
+	int array[100] = { 0 };
+	printf("Type 1 for \" first class\"\n");
+	printf("Type 2 for \"Economy class\"\n");
+	printf("select type of class: ");
+	scanf_s("%d", &select);
+	    if (select == 1) {
+		printf("please select a seat number(1-30) ");
+		scanf_s("%d", &a);
+		if (a > 30) {
+			printf("please enter apprpriate seat  num\n");
+			scanf_s("%d", &a);
+		}
+		if (array[a] == 0) {
+			printf("seat is available\n");
+			printf("your request is accepted\nyour boarding pass is here\n");
+			array[a]=1;
+			printf("======================================\n");
+			printf("|| your seat number is : %d           ||\n",a);
+			printf("|| you're travelling in first class  ||\n");
+			printf("======================================\n");
+		}	
+	
+	if (array[a] == 1)
+	{
+		printf("seat is booked\n");
+	}
+	}
+}
