47dad6aeef8179eb95ab718355252de7f9d20652
EshaTariq07 <116877833+EshaTariq07@users.noreply.github.com>
2022-10-31 20:37:18 +0500

    Add files via upload
    added initial commit

---
+++ Airline Reservation System1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<string.h>
+int main(void) {
+	int select;
+	int a;
+	int array[100] = { 0 };
+	printf("Type 1 for \" first class\"\n");
+	printf("Type 2 for \"Economy class\"\n");
+	printf("select type of class: ");
+	scanf_s("%d", &select);
+}
